d474c14fa4393e14d4620d05f43ee6c71b169aa8
Ankur Goswami <agoswam3@ucsc.edu>
2017-01-21 12:02:23 -0800

    Update example.json, update Playlist.java object

---
+++ AppServer/src/main/java/spotlight/Playlist.java
@@ -0,0 +1,25 @@
+package spotlight;
+
+public class Playlist{
+    private final long id;
+    private final Song currentSong;
+    private final Song[] playlist;
+    
+    public Playlist(long id, Song currentSong, Song[] playlist){
+        this.id = id;
+        this.currentSong = currentSong;
+        this.playlist = playlist;
+    }
+
+    public long getId{
+        return id;
+    }
+
+    public Song getCurrentSong{
+        return currentSong;
+    }
+
+    public Song[] getPlaylist{
+        return playlist;
+    }
+}
