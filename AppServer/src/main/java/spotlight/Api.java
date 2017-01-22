c01ef3216ea9d2ad0be8a2229790e9f5f19c96d5
Katerina Chinnappan <kachinna@ucsc.edu>
2017-01-21 17:28:51 -0800

    Update Api.java

--- AppServer/src/main/java/spotlight/Api.java
+++ AppServer/src/main/java/spotlight/Api.java
@@ -1,10 +1,10 @@
 import package spotlight;
 import com.wrapper.spotify.UtilProtos.Url.Scheme;
 import com.wrapper.spotify.methods.*;
-import com.wrapper.spotify.methods.authentication.AuthorizationCodeGrantRequest;
+/*import com.wrapper.spotify.methods.authentication.AuthorizationCodeGrantRequest;
 import com.wrapper.spotify.methods.authentication.AuthorizationURLRequest;
 import com.wrapper.spotify.methods.authentication.ClientCredentialsGrantRequest;
-import com.wrapper.spotify.methods.authentication.RefreshAccessTokenRequest;
+import com.wrapper.spotify.methods.authentication.RefreshAccessTokenRequest;*/
 public class Api {
 	public static final String HOST_= "api.spotify.com";
 	public static final Integer PORT_ = 443;
