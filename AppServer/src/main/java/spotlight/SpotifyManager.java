fa98bc8b66118224f64223c14ac7ea7e5297bc6b
Ankur Goswami <agoswam3@ucsc.edu>
2017-01-21 23:38:13 -0800

    Comment stuff out

--- AppServer/src/main/java/spotlight/SpotifyManager.java
+++ AppServer/src/main/java/spotlight/SpotifyManager.java
@@ -4,28 +4,28 @@ package spotlight;
 
 class SpotifyManager implements Serializable{
    
-    @GET users/{user_id}/playlists/{playlist_id}
-    public Playlist getPlaylist(){
-        // STUB
-       ArrayList<Playlist> find = new ArrayList<Playlist>;
-       //no curr user
-       if(user == NULL)
-       {
-          return null;
-       }
-       try{
-          for(Playlist playlist : )
-          {
-            Long id = playlist.getId();
-            Song currSong =  playlist.getCurrentSong();
-            List<Song>playlistt = playlist.getPlaylist();
-            find.searh(new Playlist(id, currSong, playlistt);
+    // @GET users/{user_id}/playlists/{playlist_id}
+    // public Playlist getPlaylist(){
+    //     // STUB
+    //    ArrayList<Playlist> find = new ArrayList<Playlist>;
+    //    //no curr user
+    //    if(user == NULL)
+    //    {
+    //       return null;
+    //    }
+    //    try{
+    //       for(Playlist playlist : )
+    //       {
+    //         Long id = playlist.getId();
+    //         Song currSong =  playlist.getCurrentSong();
+    //         List<Song>playlistt = playlist.getPlaylist();
+    //         find.searh(new Playlist(id, currSong, playlistt);
              
-          }
-                       }
-           catch(Exception e){
-              System.err.println("Cannot load user specified playlist");
-              e.printStackTrace();
-           }      
-    }
+    //       }
+    //                    }
+    //        catch(Exception e){
+    //           System.err.println("Cannot load user specified playlist");
+    //           e.printStackTrace();
+    //        }      
+    // }
 }
