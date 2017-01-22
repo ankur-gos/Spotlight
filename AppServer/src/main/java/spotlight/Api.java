6da56961c1783f134805a5058bf2867d6b22c91f
Katerina Chinnappan <kachinna@ucsc.edu>
2017-01-21 17:34:33 -0800

    optional

--- AppServer/src/main/java/spotlight/Api.java
+++ AppServer/src/main/java/spotlight/Api.java
@@ -6,8 +6,20 @@ import com.wrapper.spotify.methods.authentication.AuthorizationURLRequest;
 import com.wrapper.spotify.methods.authentication.ClientCredentialsGrantRequest;
 import com.wrapper.spotify.methods.authentication.RefreshAccessTokenRequest;*/
 public class Api {
-	public static final String HOST_= "api.spotify.com";
+        public static final String HOST_= "api.spotify.com";
 	public static final Integer PORT_ = 443;
+	public static final HttpManager HTTP_ = SpotifyHttpManager.builder.build();
+	public static final String AUTHENTICATION_="accounds.spotify.com";
+	public static final Scheme SCHEMA_ = Scheme.HTTPS;
+	
+	//private
+	private HttpManager httpmngr = NULL;
+	private Scheme schema;
+	print String host;
+	private int port;
+	priivate final String clientSecret;
+	private final String clientIDl
+	private final Strin URI;
 	
 
 }
