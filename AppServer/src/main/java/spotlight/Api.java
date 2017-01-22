30ed4fb81d14c5ba4f5bc4b3ac1817c0bc96cfb0
Katerina Chinnappan <kachinna@ucsc.edu>
2017-01-21 17:27:16 -0800

    option

--- AppServer/src/main/java/spotlight/Api.java
+++ AppServer/src/main/java/spotlight/Api.java
@@ -1,1 +1,13 @@
+import package spotlight;
+import com.wrapper.spotify.UtilProtos.Url.Scheme;
+import com.wrapper.spotify.methods.*;
+import com.wrapper.spotify.methods.authentication.AuthorizationCodeGrantRequest;
+import com.wrapper.spotify.methods.authentication.AuthorizationURLRequest;
+import com.wrapper.spotify.methods.authentication.ClientCredentialsGrantRequest;
+import com.wrapper.spotify.methods.authentication.RefreshAccessTokenRequest;
+public class Api {
+	public static final String HOST_= "api.spotify.com";
+	
+
+}
 
